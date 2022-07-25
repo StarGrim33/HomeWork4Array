00ae31bb54eaf22c773feb537a4b0439404cf5e2
StarGrim33 <prosky33@gmail.com>
2022-07-25 09:39:41 +0300

    5

--- Program.cs
+++ Program.cs
@@ -4,17 +4,17 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = {0};
+            int[] numbers = new int[0];
             bool exit = false;
             string sumCommand = "sum";
             string exitCommand = "exit";
 
             while (exit != true)
             {
-                Console.WriteLine("Введите число, команду sum, чтобы сложить значения ввода или команду exit, чтобы выйти.");
+                Console.WriteLine("Введите число, команду " + sumCommand + ", чтобы сложить значения ввода, или команду " + exitCommand + ", чтобы выйти.");
                 string userInput = Console.ReadLine();
 
-                if (userInput == sumCommand)                                      
+                if (userInput == sumCommand)
                 {
                     int sum = 0;
 
@@ -25,24 +25,24 @@
 
                     Console.WriteLine(sumCommand + " элементов равна: " + sum);
                 }
-                else if (userInput == exitCommand)                               
+                else if (userInput == exitCommand)
                 {
                     Console.Clear();
                     exit = true;
                     Console.SetCursorPosition(50, 5);
                     Console.WriteLine(exitCommand);
                 }
-                else                                                       
+                else
                 {
+                    int[] numbersCopy = new int[numbers.Length + 1];
                     int convertStringToNumber = Convert.ToInt32(userInput);
-                    int[] numbersCopy = { 0 };
+                    numbersCopy[numbersCopy.Length - 1] += convertStringToNumber;
 
                     for (int i = 0; i < numbers.Length; i++)
                     {
                         numbersCopy[i] = numbers[i];
                     }
 
-                    numbersCopy[numbers.Length - 1] += convertStringToNumber;
                     numbers = numbersCopy;
                 }
 
